3638fbe2a7082f640a9d1d10280de86af958ab8c
westfoldsystems <17113243+westfoldsystems@users.noreply.github.com>
2026-06-16 21:12:40 -0400

    Initial Commit

---
+++ Lantern/LanternApp.swift
@@ -0,0 +1,17 @@
+//
+//  LanternApp.swift
+//  Lantern
+//
+//  Created by jeremy mills on 6/16/26.
+//
+
+import SwiftUI
+
+@main
+struct LanternApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
